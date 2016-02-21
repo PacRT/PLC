ab8a5b9448f24238418b1023c226a8fe73d78ede
Hardik Mistry <hardik.mistry@outlook.com>
2016-02-20 19:20:29 -0600

    Horizontal Navbar enabled

--- public/js/components/navbar/app-navbar.js
+++ public/js/components/navbar/app-navbar.js
@@ -11,60 +11,85 @@ var Nav = ReactBoostrap.Nav;
 var NavItem = ReactBoostrap.NavItem;
 var MenuItem = ReactBoostrap.MenuItem;
 var NavDropdown = ReactBoostrap.NavDropdown;
+var LeftNav = require('material-ui/lib/left-nav');
+var MenuItem = require('material-ui/lib/menus/menu-item');
 var LoginStore = require('../../stores/app-login-store');
 var LoginActions = require('../../actions/app-login-actions');
-
+var NavBarActions = require('../../actions/app-navbar-actions');
 var NavBar = React.createClass({
     getInitialState: function () {
         return this.state = this._getLoginState();
     },
-    _getLoginState : function(){
+    _getLoginState: function () {
         return {
-            userLoggedIn: LoginStore.isLoggedIn()
+            userLoggedIn: LoginStore.isLoggedIn(),
+            isOpen : false
         };
     },
-    componentDidMount: function() {
+    componentDidMount: function () {
         LoginStore.addChangeListener(this._onChange);
     },
 
-    componentWillUnmount: function() {
+    componentWillUnmount: function () {
+        this.setState({
+           isOpen : false
+        });
         LoginStore.removeChangeListener(this._onChange);
     },
 
-    _onChange: function() {
+    _onChange: function () {
         this.setState(this._getLoginState());
     },
-    logout:function(event) {
+    logout: function (event) {
         event.preventDefault();
         LoginActions.logoutUser();
     },
-    getBrandName:function(){
+    toggleNav : function(event){
+        this.setState({
+            isOpen : true
+        });
+    },
+    getBrandName: function () {
         if (!this.state.userLoggedIn) {
             return (
-                <a href="#"><i className="fa fa-paper-plane-o"></i>PaperLess Club</a>
+                <Link to="/"><i className="fa fa-paper-plane-o"></i>PaperLess Club</Link>
             )
-        }else{
+        } else {
             return (
-                <a href="#/dashboard"><i className="fa fa-paper-plane-o"></i>PaperLess Club</a>
+                <Link to="/dashboard"><i className="fa fa-paper-plane-o"></i>PaperLess Club</Link>
             )
         }
     },
-    getNavBar : function(){
+    _goToLocation : function (location) {
+        NavBarActions.goToLocation(location)
+    },
+    getNavBar: function () {
         if (!this.state.userLoggedIn) {
-                return ( "" )
-            } else {
-                return (
+            return ( "" )
+        } else {
+            return (
+                <Nav>
+                    {/* <ul>
+                        <li><Link to="/mydocs">My Docs</Link></li>
+                        <li><Link to="/howto">How to</Link></li>
+                        <li><Link to="/invite">Invite</Link></li>
+                        <li><Link to="/devzone">Dev Zone</Link></li>
+                        <li><Link to="/mythings">My Things</Link></li>
+                        <li><Link to="/logout" onClick={this.logout}>Log out</Link></li>
+                    </ul>*/}
                     <Nav>
-                        <NavItem eventKey={1} href="#/mydocs">My Docs</NavItem>
-                        <NavItem eventKey={2} href="#/howto">How to</NavItem>
-                        <NavItem eventKey={2} href="#/invite">Invite</NavItem>
-                        <NavItem eventKey={2} href="#/devzone">Dev Zone</NavItem>
-                        <NavItem eventKey={2} href="#/mythings">My Things</NavItem>
-                        <NavItem eventKey={2} onClick={this.logout} href="#/logout">Logout</NavItem>
+                        <NavItem eventKey={1} href="#" onClick={this._goToLocation.bind(null, 'mydocs')}>My Docs</NavItem>
+                        <NavItem eventKey={2} href="#" onClick={this._goToLocation.bind(null, 'howto')}>How to</NavItem>
+                        <NavItem eventKey={2} href="#" onClick={this._goToLocation.bind(null, 'invite')}>Invite</NavItem>
+                        <NavItem eventKey={2} onClick={this._goToLocation.bind(null, 'devzone')}>Dev Zone</NavItem>
+                        <NavItem eventKey={2} onClick={this._goToLocation.bind(null, 'mythings')}>My Things</NavItem>
+                        <NavItem eventKey={2} onClick={this.logout}>Logout</NavItem>
                     </Nav>
-                )
+
+                </Nav>
+            )
         }
-     },
+    },
     render: function () {
         return (
             <Navbar inverse className="navbar-fixed-top">
