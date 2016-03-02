26208178181b8fc4fa4776fa8347e2e0fbdf3940
Hardik Mistry <hardik.mistry@outlook.com>
2016-03-01 20:31:58 -0600

    added script to create vmail directory

--- pythonscript/createUserVmail.py
+++ pythonscript/createUserVmail.py
@@ -1,6 +1,24 @@
-import mysql.connector
-cnx = mysql.connector.connect(user='postfix', password='postfix',
-                              host='http://paperlessclub.org',
-                              database='postfix')
-println("closing connection")
-cnx.close()
+from __future__ import print_function
+from datetime import date, datetime, timedelta
+import mysql.connector,redis
+
+r = redis.Redis()
+p = r.pubsub()
+p.subscribe("createUserDirectory")
+
+def createUserDirectory(json_msg):
+    cnx = mysql.connector.connect(user=‘postfix', password=‘postfix’ database=‘postfix')
+    cursor = cnx.cursor()
+    user_name = json_msg['user_name']
+    add_user = ("INSERT INTO mailbox (email, maidir) VALUES (%s, %s)")
+    data_user = (user_name+'@paperlessclub.org', user_name+'/')
+    cursor.execute(add_user, data_user)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+for message in p.listen():
+    createUserDirectory(message)
+
+p.close()
+
