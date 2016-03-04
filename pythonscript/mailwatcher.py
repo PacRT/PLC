543e8e832d60d8c29c27469fc23f1fb63db55470
Hardik Mistry <hardik.mistry@outlook.com>
2016-03-03 20:08:19 -0600

    corrected indentation

--- pythonscript/mailwatcher.py
+++ pythonscript/mailwatcher.py
@@ -10,61 +10,61 @@ import requests
 import updateredis
 
 def getFileId():
-  rawreply = urllib.request.urlopen('http://50.250.218.65:9333/dir/assign')
-  content = rawreply.read()
-  data = json.loads(content.decode('utf8'))
-  pprint(data)
-  fid = data["fid"]
-  url = data["url"]
-  final_url = "http://" + url + "/" + fid
-  print(final_url)
-  return final_url
+    rawreply = urllib.request.urlopen('http://50.250.218.65:9333/dir/assign')
+    content = rawreply.read()
+    data = json.loads(content.decode('utf8'))
+    pprint(data)
+    fid = data["fid"]
+    url = data["url"]
+    final_url = "http://" + url + "/" + fid
+    print(final_url)
+    return final_url
 
 def Load(type, fname, owner, issuer, issdname):
-  url = getFileId()
-  files = {'file': open(fname, 'rb')}
-  r = requests.post(url, files=files)
-  print("File is uploading the type is: ", type)
-  extension = type.split('/')[1]
-  print("Extension: " + extension)
-  finalurl = url+ "." +extension
-  print("Final URL: " + finalurl)
-  print(r.text)
-  doc_url = "/docs/"+finalurl.split("//")[1].replace(":",'/')
-  print(doc_url)
-  updateredis.update_redis(owner, issuer, finalurl, issdname,fname,doc_url)
+    url = getFileId()
+    files = {'file': open(fname, 'rb')}
+    r = requests.post(url, files=files)
+    print("File is uploading the type is: ", type)
+    extension = type.split('/')[1]
+    print("Extension: " + extension)
+    finalurl = url+ "." +extension
+    print("Final URL: " + finalurl)
+    print(r.text)
+    doc_url = "/docs/"+finalurl.split("//")[1].replace(":",'/')
+    print(doc_url)
+    updateredis.update_redis(owner, issuer, finalurl, issdname,fname,doc_url)
 
 def processPart(part, owner, issuer, issdname):
-  ctype = part.get_content_type()
-  print("CTYPE: " +ctype)
-  if ctype in ['image/jpeg','image/jpg', 'image/png','application/pdf']:
-    fw = open(issuer + ":" + owner + ":" + part.get_filename(), 'wb')
-    fw.write(part.get_payload(decode=True))
-    fname=fw.name
-    fw.close()
-    print(fname)
-    Load(ctype, fname, owner, issuer, issdname)
+    ctype = part.get_content_type()
+    print("CTYPE: " +ctype)
+    if ctype in ['image/jpeg','image/jpg', 'image/png','application/pdf']:
+        fw = open(issuer + ":" + owner + ":" + part.get_filename(), 'wb')
+        fw.write(part.get_payload(decode=True))
+        fname=fw.name
+        fw.close()
+        print(fname)
+        Load(ctype, fname, owner, issuer, issdname)
 
 def ExtractAndLoad(obj):
-  fp = email.parser.BytesFeedParser()
-  try:
-    fp.feed(open(obj, "rb").read())
-    msg = fp.close()
-    #if msg.is_multipart():
-    #print('multipart')
-    tovar  = msg['to']
-    owner = email.utils.parseaddr(tovar)[1]
-    print(owner)
-    fromvar = msg['from']
-    issdname = email.utils.parseaddr(fromvar)[0]
-    print(issdname)
-    issuer = email.utils.parseaddr(fromvar)[1]
-    print(issuer)
-    for part in msg.walk():
-    processPart(part, owner, issuer, issdname)
-  except IsADirectoryError:
-    print("No Need to parse the event..")
-    print("New Directory created!")
+    fp = email.parser.BytesFeedParser()
+    try:
+        fp.feed(open(obj, "rb").read())
+        msg = fp.close()
+        #if msg.is_multipart():
+        #print('multipart')
+        tovar  = msg['to']
+        owner = email.utils.parseaddr(tovar)[1]
+        print(owner)
+        fromvar = msg['from']
+        issdname = email.utils.parseaddr(fromvar)[0]
+        print(issdname)
+        issuer = email.utils.parseaddr(fromvar)[1]
+        print(issuer)
+        for part in msg.walk():
+            processPart(part, owner, issuer, issdname)
+    except IsADirectoryError:
+        print("No Need to parse the event..")
+        print("New Directory created!")
 
 
 wm = pyinotify.WatchManager()  # Watch Manager
