8439f49ec675ba1d623488cc3fe60281253fb4a3
Hardik Mistry <hardik.mistry@outlook.com>
2016-03-22 17:33:48 -0500

    #38 reorganized input to lua script

--- routes/api/docs/edit_doc_metadata.js
+++ routes/api/docs/edit_doc_metadata.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var docs_api = require('../../redis_middleware/api/redis_docs_api');
 var error_codes = require('../../constants/error-constants');
 
-router.post('/updateMetaData/', function(req, res, next){
+router.post('/updateMetaData', function(req, res, next){
     var doc_url = req.body['doc_url'];
     doc_url = doc_url.split("docs")[1].split("/");
-    doc_url = "http://" + doc_url[0] + ":" + doc_url[1] + "/" + doc_url[2];
-    docs_api.updateDocMetaData(doc_url, req.body['category'], req.body['file_name'], req.body['username']).then(function(response){
+    doc_url = "http://" + doc_url[1] + ":" + doc_url[2] + "/" + doc_url[3];
+    docs_api.updateDocMetaData(doc_url, req.body['category'], req.body['file_name'], req.headers['user_name']).then(function(response){
         res.send({
           'message': 'Document meta updated successfully'
         });
