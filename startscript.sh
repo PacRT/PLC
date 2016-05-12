5b328d3892f8b4eab684b6d1d21df1d148105aad
Mistry, Hardik <hardik.mistry@cirrus.com>
2016-05-12 00:17:06 -0500

    checked in startup script

---
+++ startscript.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+echo "\nrunning ... script"
+echo $PWD
+
+echo "********************************************************** Starting generate_super_user"
+sh shellscript/generate_super_user.sh
+echo "********************************************************** Ending generate_super_user"
+
+echo "********************************************************** Starting Node app and UI"
+
+echo 'hardik123' | sudo -S command
+cd pythonscript
+chmod +x confirmemail.py
+chmod +x createUserVmail.py
+chmod +x forwardPackage.py
+chmod +x add_package.py
+sudo python add_package.py &
+sudo python confirmemail.py &
+sudo python createUserVmail.py &
+sudo python forwardPackage.py &
+cd ..
+echo $PWD
+echo "kicking off server"
+gulp server node-app --prod
+
+echo "********************************************************** Ending"
+
+# docker build -t plc .
+# docker run -i -t -p 3333:3333 -p 7979:7979 -p 6379:6379 plchardik@deagle:~/stash/plc_alpha$
