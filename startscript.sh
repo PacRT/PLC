42f6c4cc9ee5cddb85021c1aacf4fd2fa36b3d14
Mistry, Hardik <hardik.mistry@cirrus.com>
2016-05-13 10:57:36 -0500

    updated startup script

--- startscript.sh
+++ startscript.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 echo "\nrunning ... script"
 echo $PWD
-echo "Killing Front End and Nodejs process---started"
-$react_pid = lsof -ti tcp:7979
-kill -9 $react_pid
-$node_pid = lsof -ti tcp:3333
-kill -9 $node_pid
-$weed_pid = lsof -ti tcp:9333
-kill -9 $weed_pid
-echo "Killing Front End and Nodejs process---ended"
 echo "********************************************************** Starting generate_super_user"
 sh shellscript/generate_super_user.sh
 echo "********************************************************** Ending generate_super_user"
 
 echo "********************************************************** Starting Node app and UI"
 echo 'hardik123' | sudo -S command
+
+echo "Killing Front End and Nodejs process---started"
+react_pid = sudo lsof -ti tcp:7979
+kill -9 $react_pid
+node_pid = sudo lsof -ti tcp:3333
+kill -9 $node_pid
+weed_pid = sudo  -ti tcp:9333
+kill -9 $weed_pid
+echo "Killing Front End and Nodejs process---ended"
+
 sudo weed server -master.port=9333 -volume.port=8080 -dir="/data"
 cd pythonscript
 chmod +x confirmemail.py
