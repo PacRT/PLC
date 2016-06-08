6e2bc25269c010fd4683c53892e54833e8c23cfe
Mistry, Hardik <hardik.mistry@cirrus.com>
2016-06-08 00:20:42 -0500

    sending actual data from cassnadra

--- routes/api/inbox/inbox_get_threads.js
+++ routes/api/inbox/inbox_get_threads.js
@@ -7,8 +7,11 @@ var inbox_api = require('../../cassandra_middleware/cassandra_inbox_api');
 var error_codes = require('../../constants/error-constants');
 var inbox_data = require('./inbox_data');
 router.get('/getThreads',function(req, res, next){
-    inbox_api.get_threads(req.body.data).then(function(result){
-        res.send(inbox_data);
+    var data = {
+        "user_id" : req.headers["user_name"]
+    };
+    inbox_api.get_threads(data).then(function(result){
+        res.send(result);
     },function(error){
         res.status(600).send(JSON.stringify({error:true,"errorMsg":error_codes[error]}));
     });
