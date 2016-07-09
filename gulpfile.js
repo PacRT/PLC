2eeb2ce828228fcf1bd8dcd0d3d79756f5a4fcba
Mistry, Hardik <hardik.mistry@cirrus.com>
2016-07-09 17:35:49 -0500

    refactored gulp file.. faster bundling and optimized magnification

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,29 @@
-var concat = require('gulp-concat'),
-    gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    nodemon = require('gulp-nodemon'),
-    watchify = require('watchify'),
-    reactify = require('reactify'),
-    minify = require('gulp-minify'),
-    argv = require('yargs').argv,
-    gulpif = require('gulp-if'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify');
-var browserify = require('browserify');
-var uglify = require('gulp-uglify');
+var gulp = require('gulp');
 var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
-var assign = require('lodash.assign');
-var gutil = require('gulp-util');
-var sourcemaps = require('gulp-sourcemaps');
-var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
 var notify = require('gulp-notify');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var gutil = require('gulp-util');
+var glob = require('glob');
 var livereload = require('gulp-livereload');
 var connect = require('gulp-connect');
-var replace = require('gulp-replace');
+var rename = require('gulp-rename');
 var historyApiFallback = require('connect-history-api-fallback');
+var replace = require('gulp-replace');
 
-var util = require('util');
-var exec = require('child_process').exec;
-var root_js_path = './bower_components/';
-var root_css_path = './public/assets/css/';
+// External dependencies you do not want to rebundle while developing,
+// but include in your application deployment
+var dependencies = [
+    'jquery',
+    'react',
+    'materialize-tags'
+];
 var PROD_CONSTANTS = {
     "NODE_SERVER" : "paperlessclub.org",
     "NODE_PORT"   : 3333,
@@ -37,146 +34,194 @@ var DEV_CONSTANTS = {
     "NODE_PORT"   : 3333,
     "SEAWEEDFS_ENDPOINT" : "127.0.0.1:9333"
 };
-var vendor_js_path = [
-    "./node_modules/pdfjs-dist/build/pdf.combined.js",
-    root_js_path + "jquery/dist/jquery.min.js",
-    "./node_modules/materialize-tags/dist/js/materialize-tags.min.js",
-    root_js_path + "react/react.min.js",
-    root_js_path + "react/react-dom.min.js",
-    root_js_path + "react/react-dom-server.min.js",
-    root_js_path + "react/react-with-addons.min.js",
+var browserifyTask = function (options) {
 
-];
+    // Our app bundler
+    var appBundler = browserify({
+        entries: [options.src], // Only need initial file, browserify finds the rest
+        transform: [[babelify, {presets: ['es2015','react']}]], // We want to convert JSX to normal javascript
+        debug: options.development, // Gives us sourcemapping
+        cache: {}, packageCache: {}, fullPaths: options.development // Requirement of watchify
+    });
 
-var vendor_css_path = [
+    // We set our dependencies as externals on our app bundler when developing
+    (options.development ? dependencies : []).forEach(function (dep) {
+        appBundler.external(dep);
+    });
+
+    // The rebundle process
+    var rebundle = function () {
+        var start = Date.now();
+        console.log('Building APP bundle');
+        appBundler.bundle()
+            .on('error', gutil.log)
+            .pipe(source('app.js'))
+            .pipe(replace("#NODE_SERVER#", DEV_CONSTANTS.NODE_SERVER))
+            .pipe(replace("#NODE_PORT#"  , DEV_CONSTANTS.NODE_PORT  ))
+            .pipe(gulpif(!options.development,replace("#NODE_SERVER#", PROD_CONSTANTS.NODE_SERVER)))
+            .pipe(gulpif(!options.development,replace("#NODE_PORT#", PROD_CONSTANTS.NODE_PORT)))
+            .pipe(gulpif(!options.development, streamify(uglify())))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(options.dest))
+            .pipe(gulpif(options.development, livereload()))
+            .pipe(notify(function () {
+                console.log('APP bundle built in ' + (Date.now() - start) + 'ms');
+            }));
+    };
+
+    // Fire up Watchify when developing
+    if (options.development) {
+        appBundler = watchify(appBundler);
+        appBundler.on('update', rebundle);
+    }
+
+    rebundle();
+
+    // We create a separate bundle for our dependencies as they
+    // should not rebundle on file changes. This only happens when
+    // we develop. When deploying the dependencies will be included
+    // in the application bundle
+
+
+        var vendorsBundler = browserify({
+            debug: true,
+            require: dependencies
+        });
+
+        // Run the vendor bundle
+        var start = new Date();
+        console.log('Building VENDORS bundle');
+        vendorsBundler.bundle()
+            .on('error', gutil.log)
+            .pipe(source('vendors.js'))
+            .pipe(gulpif(!options.development, streamify(uglify())))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(options.dest))
+            .pipe(notify(function () {
+                console.log('VENDORS bundle built in ' + (Date.now() - start) + 'ms');
+            }));
+
+
+}
+
+var cssTask = function (options) {
+    if (options.development) {
+        var run = function () {
+            console.log(arguments);
+            var start = new Date();
+            console.log('Building CSS bundle');
+            gulp.src(options.src)
+                .pipe(concat('app.css'))
+                .pipe(rename({suffix: '.min'}))
+                .pipe(gulp.dest(options.dest))
+                .pipe(notify(function () {
+                    console.log('CSS bundle built in ' + (Date.now() - start) + 'ms');
+                }));
+        };
+        run();
+        gulp.watch(options.src, run);
+    } else {
+        gulp.src(options.src)
+            .pipe(concat('app.css'))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(cssmin())
+            .pipe(gulp.dest(options.dest));
+    }
+}
+
+// Starts our development workflow
+var root_css_path = './public/assets/css/';
+var css_path = [
     root_css_path + "vendorcss/" + "/bootstrap/bootstrap.min.css",
     root_css_path + "vendorcss/" + "/font-awesome/font-awesome.min.css",
-   "./node_modules/materialize-tags/dist/css/materialize-tags.min.css"
-];
-
-var app_css_path = [
+    "./node_modules/materialize-tags/dist/css/materialize-tags.min.css",
     root_css_path + "registration.css",
     root_css_path + "base.css"
-]
-gulp
-    //Bundle all Third Party Plugins
-    .task('vendor_js',function(){
-        gulp.src(vendor_js_path)
-        .pipe(concat('vendor.min.js'))
-        .pipe(gulp.dest('dist/assets'));
-    })
+];
 
-    //Bundle all Third Party CSS
-    .task('vendor_css',function(){
-        gulp.src(vendor_css_path)
-        .pipe(concat('vendor.css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist/assets/css/vendorcss'))
-    })
-    .task('transform', function(){
-        var start = Date.now();
-        var opts = {
-            entries: ['public/js/app.js'],
-            cache: {},
-            packageCache: {},
-            transform : [reactify],
-            debug : argv.prod ? false : true ,
-            fullPaths : argv.prod ? false : true
-        };
-        var appBundler = browserify(opts);
-        var bundler = function(){
-            appBundler.bundle()
-                .on('error', gutil.log)
-                .pipe(source('app.js'))
-                //.pipe(gulpif(!argv.prod,sourcemaps.init()))
-                // Add transformation tasks to the pipeline here.
-                .pipe(gulpif(argv.prod, (replace("#NODE_SERVER#", PROD_CONSTANTS.NODE_SERVER))))
-                .pipe(gulpif(argv.prod, (replace("#NODE_PORT#"  , PROD_CONSTANTS.NODE_PORT  ))))
-                .pipe(replace("#NODE_SERVER#", DEV_CONSTANTS.NODE_SERVER))
-                .pipe(replace("#NODE_PORT#"  , DEV_CONSTANTS.NODE_PORT  ))
-                .pipe(rename({suffix: '.min'}))
-                //.pipe(gulpif(!argv.prod,sourcemaps.write('./')))
-                .pipe(gulp.dest('dist/js/'))
-                .pipe(livereload())
-                .pipe(notify(function () {
-                    console.log('APP bundle built in ' + (Date.now() - start) + 'ms');
-                }));
-        }
-        if (!argv.prod) {
-            b = watchify(appBundler);
-            appBundler.on('update', bundler);
-        }
-        bundler();
-    })
-    // moves source files to dist
-    .task('copy', function(){
-        gulp
-            .src('public/index.html')
-            .pipe(gulp.dest('dist'));
-        gulp
-            .src('public/assets/css/images/*.*')
-            .pipe(gulp.dest('dist/assets/css/images'));
-        gulp
-            .src('public/assets/css/vendorcss/fonts/*.*')
-            .pipe(gulp.dest('dist/assets/css/fonts'))
-        gulp
-            .src(app_css_path)
-            .pipe(concat('app.css'))
-            //s.pipe(gulpif(argv.prod, uglify())).on('error', gutil.log)
-            .pipe(rename({suffix: '.min'}))
-            .pipe(gulp.dest('dist/assets/css'));
 
-    })
+gulp.task('copy-static',function(){
+    gulp
+        .src('public/index.html')
+        .pipe(gulp.dest('dist'));
+    gulp
+        .src('public/assets/css/images/*.*')
+        .pipe(gulp.dest('dist/assets/css/images'));
+    gulp
+        .src('public/assets/css/vendorcss/fonts/*.*')
+        .pipe(gulp.dest('dist/assets/fonts'));
+});
+var nodemon = require('gulp-nodemon');
 
-    // node api Server
-    .task('node-app', function () {
-        if(argv.prod){
-            gulp.src('routes/constants/constant.js')
-                .pipe(replace("#SEAWEEDFS_ENDPOINT#",PROD_CONSTANTS.SEAWEEDFS_ENDPOINT))
-                .pipe(gulp.dest('routes/constants'));
-        }else{
-            gulp.src('routes/constants/constant.js')
-                .pipe(replace("#SEAWEEDFS_ENDPOINT#",DEV_CONSTANTS.SEAWEEDFS_ENDPOINT))
-                .pipe(gulp.dest('routes/constants'));
+function startNodeApp(options){
 
-        }
-        nodemon({ script: 'routes/node-app.js'})
+    gulp.src('routes/constants/constant.js')
+        .pipe(replace("#SEAWEEDFS_ENDPOINT#",DEV_CONSTANTS.SEAWEEDFS_ENDPOINT))
+        .pipe(gulpif(!options.development,replace("#SEAWEEDFS_ENDPOINT#", PROD_CONSTANTS.NODE_SERVER)))
+        .pipe(gulp.dest('routes/constants'));
+    nodemon({ script: 'routes/node-app.js'})
         .on('restart', function () {
             console.log('restarted!')
         })
-    })
-    .task('server', ['vendor_css','vendor_js','transform','copy'], function() {
-        livereload.listen();
-        connect.server({
-            root: 'dist/',
-            livereload : !argv.prod,
-            port: 7979,
-            middleware: function(connect, opt){
-                console.log("in history fallback!!!")
-                return [historyApiFallback({
-                    logger: console.log.bind(console)
-                })];
-            }
-        });
-    }).
+}
 
-    task("redis-server",function(){
-        exec('redis-server',function (error, stdout, stderr) {
-           console.log(stdout);
-            if (error !== null) {
-                console.log('exec error: ' + error);
-            }
-        });
-    }).
+gulp.task('default',['copy-static'], function () {
+    livereload.listen();
 
-    task("seaweedfs-server",function(){
-        exec('./seaweedfs/weed server -master.port=9333 -volume.port=8080 -dir="./seaweedfs/data"',function (error, stdout, stderr) {
-            sys.print('stdout: ' + stdout);
-            sys.print('stderr: ' + stderr);
-            if (error !== null) {
-                console.log('exec error: ' + error);
-            }
-        });
+    browserifyTask({
+        development: true,
+        src: 'public/js/app.js',
+        dest: 'dist/js/'
     });
 
 
+    cssTask({
+        development: true,
+        src: css_path,
+        dest: 'dist/assets/css'
+    });
+
+    connect.server({
+        root: 'dist',
+        port: 7979,
+        middleware: function(connect, opt){
+            console.log("in history fallback!!!")
+            return [historyApiFallback({
+            })];
+        }
+    });
+    startNodeApp({
+        development : true
+    })
+
+
+});
+gulp.task('deploy', ['copy-static'] , function () {
+
+    browserifyTask({
+        development: false,
+        src: 'public/js/app.js',
+        dest: 'dist/js/'
+    });
+
+    cssTask({
+        development: false,
+        src:css_path ,
+        dest: 'dist/assets/css'
+    });
+
+    connect.server({
+        root: 'dist',
+        port: 7979,
+        middleware: function(connect, opt){
+            console.log("in history fallback!!!")
+            return [historyApiFallback({
+            })];
+        }
+    });
+
+    startNodeApp({
+        development : false
+    })
+
+});
