33b48530691a1cfd3e8888e2f016e02b02fbed5f
Mistry, Hardik <hardik.mistry@cirrus.com>
2016-07-09 19:20:18 -0500

    adding ssl certificate to make connection secure

---
+++ routes/secure-node-app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by hmistry on 7/9/16.
+ */
+var fs = require('fs'),
+    https = require('https'),
+    express = require('express'),
+    app = express();
+
+
+var privateKey  = fs.readFileSync(__dirname+'/../sslcert/key.pem', 'utf8');
+var certificate = fs.readFileSync(__dirname+'/../sslcert/cert.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
+https.createServer(credentials, app,function(){
+    console.log("secure server started on 8181");
+}).listen(8181);
+
+app.get('/', function (req, res) {
+    res.header('Content-type', 'text/html');
+    return res.end('<h1>Hello, Secure World!</h1>');
+});
