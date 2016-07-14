b33b8704d0bf39a19341317b4a28e04c78200c22
Mistry, Hardik <hardik.mistry@cirrus.com>
2016-07-14 13:10:20 -0500

    secure plc server

--- plc-server.js
+++ plc-server.js
@@ -5,15 +5,20 @@ var express = require('express');
 var path = require('path');
 var port = 7979;
 var app = express();
-
+var fs = require('fs');
+var https = require('https');
 // serve static assets normally
 app.use(express.static(__dirname + '/dist'))
 
-// handle every other route with index.html, which will contain
-// a script tag to your application's JavaScript file(s).
-app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-})
+var privateKey  = fs.readFileSync(__dirname+'/sslcert/key.pem', 'utf8');
+var certificate = fs.readFileSync(__dirname+'/sslcert/cert.pem', 'utf8');
 
-app.listen(port);
-console.log("server started on port " + port);
+var credentials = {key: privateKey, cert: certificate};
+
+https.createServer(credentials, app,function(){
+    console.log("secure server started on 8181");
+}).listen(8181);
+
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
