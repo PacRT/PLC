0ab42e1879169feef7be1e9a34cd283d491ccbdd
Mistry, Hardik <hardik.mistry@cirrus.com>
2016-07-18 19:49:02 -0500

    updated uservmail script

--- pythonscript/createUserVmail.py
+++ pythonscript/createUserVmail.py
@@ -1,27 +1,20 @@
 from __future__ import print_function
 from datetime import date, datetime, timedelta
-import mysql.connector,redis,json
+import mysql.connector,json
+import zerorpc
 
-r = redis.Redis()
-p = r.pubsub()
-p.subscribe("createUserDirectory")
 
-def createUserDirectory(json_msg):
-    cnx = mysql.connector.connect(user='postfix', password='postfix', database='postfix')
-    cursor = cnx.cursor()
-    if json_msg == 1:
-       return
-    json1 = json.loads(json_msg)
-    user_name = json1['user_name']
-    add_user = ("INSERT INTO mailbox (email, maildir) VALUES (%s, %s)")
-    data_user = (user_name+'@paperlessclub.org', user_name+'/')
-    cursor.execute(add_user, data_user)
-    cnx.commit()
-    cursor.close()
-    cnx.close()
+class createUserDirectory(object):
+    def createDirectory(self, user_name, email):
+        cnx = mysql.connector.connect(user='postfix', password='postfix', database='postfix')
+        cursor = cnx.cursor()
+        add_user = ("INSERT INTO mailbox (email, maildir) VALUES (%s, %s)")
+        data_user = (user_name+'@paperlessclub.org', user_name+'/')
+        cursor.execute(add_user, data_user)
+        cnx.commit()
+        cursor.close()
+        cnx.close()
 
-for message in p.listen():
-    createUserDirectory(message['data'])
-
-p.close()
-
+rpc_server = zerorpc.Server(createUserDirectory())
+rpc_server.bind("tcp://0.0.0.0:4242")
+rpc_server.run()
