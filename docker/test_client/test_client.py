3a59a8fe008f9dd04b40b1e753c870552d05ddd4
Mistry, Hardik <hardik.mistry@cirrus.com>
2016-07-18 19:48:40 -0500

    executing docker from root

--- docker/test_client/test_client.py
+++ docker/test_client/test_client.py
@@ -2,6 +2,6 @@ import zerorpc
 
 c = zerorpc.Client()
 c.connect("tcp://50.250.218.65:4242")
-print c.hello("this is working!!!!")
+print c.hello({'name' : 'hardik'})
 
 #/usr/local/lib/python2.7.10/bin/python -V
