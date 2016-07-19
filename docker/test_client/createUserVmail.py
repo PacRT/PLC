9c3d848446713458ca4bc493a566acf3b0b78259
Mistry, Hardik <hardik.mistry@cirrus.com>
2016-07-18 19:46:09 -0500

    moved createUserVmail script to docker folder

---
+++ docker/test_client/createUserVmail.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+from datetime import date, datetime, timedelta
+import mysql.connector,json
+import zerorpc
+
+
+class createUserDirectory(object):
+    def createDirectory(self, user_name, email):
+        cnx = mysql.connector.connect(user='postfix', password='postfix', database='postfix')
+        cursor = cnx.cursor()
+        add_user = ("INSERT INTO mailbox (email, maildir) VALUES (%s, %s)")
+        data_user = (user_name+'@paperlessclub.org', user_name+'/')
+        cursor.execute(add_user, data_user)
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+
+rpc_server = zerorpc.Server(createUserDirectory())
+rpc_server.bind("tcp://0.0.0.0:4242")
+rpc_server.run()
