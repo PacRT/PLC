f141ae211ca80756605ed863d10dc44d1aa095ad
Hardik Mistry <hardik.mistry@outlook.com>
2016-10-22 13:26:17 -0500

    proxying to plc-api server. instead of directly calling it from browser

--- plc-server.js
+++ plc-server.js
@@ -8,17 +8,24 @@ var app = express();
 var fs = require('fs');
 var https = require('https');
 // serve static assets normally
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer();
+
 app.use(express.static(__dirname + '/dist'))
 
 var privateKey  = fs.readFileSync(__dirname+'/sslcert/key.pem', 'utf8');
 var certificate = fs.readFileSync(__dirname+'/sslcert/cert.pem', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
-
-https.createServer(credentials, app,function(){
-    console.log("secure server started on 8181");
+app.use('/api/v1/*', function(req, res, next){
+    proxy.web(req, res, {
+        target: 'https://127.0.0.1:3333'+req.originalUrl,
+        secure: false
+    }, next);
+});
+https.createServer(credentials, app,function(req, res){
+    console.log("PLC server started at 7979");
 }).listen(7979);
-
 app.get('*', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
